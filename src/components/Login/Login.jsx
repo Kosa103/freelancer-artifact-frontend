24ddd922e53f803cdbc704a2721b79c256724d4f
Tamás Kósa <dotomiro2@gmail.com>
2022-04-26 22:16:15 +0200

    master Add routing and icons

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Login.scss';
+
+export const Login = () => {
+    return (
+        <div>
+            LOGIN    
+        </div>
+    );
+};
+
+export default Login;
